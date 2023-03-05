6e64067a4cc91a2d54daef72f11342e59f61ad78
Kishan <kishanknows@gmail.com>
2023-03-05 06:19:40 +0530

    changed splash screen implementation #4

--- src/screens/intro.tsx
+++ src/screens/intro.tsx
@@ -1,29 +1,38 @@
-import Video from 'react-native-video';
-import {View, StatusBar} from 'react-native';
-import auth from '@react-native-firebase/auth';
+import {View, StatusBar, Image, StyleSheet} from 'react-native';
+import {useEffect} from 'react';
+import {useSelector} from 'react-redux';
 
 const IntroScreen = ({navigation}) => {
-  const isLoggedIn = auth().currentUser;
+  const isLoggedIn = useSelector(state => state.userConfig.uid);
+
+  useEffect(() => {
+    setTimeout(() => {
+      isLoggedIn ? navigation.replace('Home') : navigation.replace('Login');
+    }, 2700);
+  }, []);
+
   return (
-    <View
-      style={{
-        justifyContent: 'center',
-        backgroundColor: 'white',
-        height: '100%',
-      }}>
+    <View style={styles.background}>
       <StatusBar
-        backgroundColor="white"
-        barStyle="dark-content"
+        backgroundColor="#492849"
+        barStyle="light-content"
         translucent={true}
       />
-      <Video
-        source={require('../assets/intronew.mp4')}
-        style={{width: '100%', height: '100%'}}
-        onEnd={() =>
-          isLoggedIn ? navigation.replace('Home') : navigation.replace('Login')
-        }
-      />
+      <Image source={require('../assets/splash.gif')} style={styles.image} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  background: {
+    justifyContent: 'center',
+    backgroundColor: '#492849',
+    height: '100%',
+  },
+  image: {
+    width: '100%',
+    resizeMode: 'contain',
+    height: 60,
+  },
+});
 export default IntroScreen;
