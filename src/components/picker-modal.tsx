c019fc2dd8d468ae00b6abb2622c616f9097e133
Kishan Jangde <kishanknows@gmail.com>
2023-03-16 10:18:56 +0530

    changed modal implementation

---
+++ src/components/picker-modal.tsx
@@ -0,0 +1,75 @@
+import Modal from 'react-native-modal';
+import {View, StyleSheet, Text} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import {RootState} from '../redux/store';
+import {showPickerModal, showUploadModal} from '../redux/general-slice';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import addImage from '../features/add-image/addImage';
+import {setImage} from '../features/add-image/imageSlice';
+
+const PickerModal = () => {
+  const dispatch = useDispatch();
+  const pickerModalShown = useSelector(
+    (state: RootState) => state.general.pickerModalShown,
+  );
+
+  const imgSelector = (mode: 'camera' | 'storage') => {
+    addImage(mode)
+      .then(res => {
+        dispatch(setImage(res.assets[0]));
+        dispatch(showPickerModal(false));
+        dispatch(showUploadModal(true));
+      })
+      .catch(err => dispatch(showPickerModal(false)));
+  };
+
+  return (
+    <Modal
+      isVisible={pickerModalShown}
+      onBackdropPress={() => dispatch(showPickerModal(false))}>
+      <View style={styles.container}>
+        <Text style={styles.modalText}>Choose from gallery or Click now!</Text>
+        <View style={styles.iconView}>
+          <MaterialIcon
+            name="add-photo-alternate"
+            size={30}
+            color="black"
+            onPress={() => imgSelector('storage')}
+          />
+          <MaterialIcon
+            name="add-a-photo"
+            size={30}
+            color="black"
+            onPress={() => imgSelector('camera')}
+          />
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: 120,
+    width: 220,
+    backgroundColor: '#f6eff6',
+    borderRadius: 20,
+    alignSelf: 'center',
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'white',
+  },
+  modalText: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: 'black',
+    textAlign: 'center',
+  },
+  iconView: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 20,
+  },
+});
+
+export default PickerModal;
---
+++ src/components/upload-modal.tsx
@@ -0,0 +1,101 @@
+import Modal from 'react-native-modal';
+import {View, StyleSheet, Text, ActivityIndicator} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import {RootState} from '../redux/store';
+import {showUploadModal} from '../redux/general-slice';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons';
+import {unsetImage} from '../features/add-image/imageSlice';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+import {useState} from 'react';
+import postImage from '../utils/post-image';
+import updateProfile from '../utils/update-profile';
+import {setUser} from '../features/auth/authSlice';
+import auth from '@react-native-firebase/auth';
+
+const UploadModal = props => {
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+  const uploadModalShown = useSelector(
+    (state: RootState) => state.general.uploadModalShown,
+  );
+  const selectedImg = useSelector((state: RootState) => state.selectedImg);
+  const user = useSelector((state: RootState) => state.userConfig);
+
+  const confirm = () => {
+    setLoading(true);
+    props.route.name === 'Profile'
+      ? updateProfile(selectedImg, user.email)
+          .then(() => dispatch(setUser(auth().currentUser?.providerData[0])))
+          .finally(() => {
+            dispatch(showUploadModal(false));
+            setLoading(false);
+          })
+      : postImage(selectedImg, user).finally(() => {
+          dispatch(showUploadModal(false));
+          setLoading(false);
+        });
+  };
+
+  const discard = () => {
+    dispatch(showUploadModal(false));
+    dispatch(unsetImage());
+  };
+
+  return (
+    <Modal isVisible={uploadModalShown} onBackdropPress={discard}>
+      <View style={styles.container}>
+        {loading ? (
+          <Text style={styles.modalText}>Uploading...</Text>
+        ) : (
+          <Text style={styles.modalText}>Upload the selected Image?</Text>
+        )}
+        <View style={styles.iconView}>
+          <MaterialIcon
+            name="close"
+            size={30}
+            color="black"
+            onPress={discard}
+          />
+          {loading ? (
+            <ActivityIndicator size={30} color="black" />
+          ) : (
+            <FeatherIcon
+              name="upload"
+              size={30}
+              color="black"
+              onPress={confirm}
+            />
+          )}
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: 120,
+    width: 220,
+    backgroundColor: '#f6eff6',
+    borderRadius: 20,
+    alignSelf: 'center',
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'white',
+  },
+  modalText: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: 'black',
+    textAlign: 'center',
+    flex: 2,
+  },
+  iconView: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 5,
+    flex: 1,
+  },
+});
+
+export default UploadModal;
